ed43de883037eb7f014d451ae56ed0c32011f133
tcox8 <tyler_c_cox@yahoo.com>
2021-03-02 08:59:22 -0500

    Update pagerduty.ps1
    Added Dynamic module importing, added dynamic variables, added logic to handle alerts with larger than 1024 characters.

--- pagerduty.ps1
+++ pagerduty.ps1
@@ -1,11 +1,45 @@
+#############################################################################
+# Contributing Author  : Tyler Cox
+# Inital Author: wilsonphan-pd (github name)
+# 
+# Special Thanks: Slightly Adapted from https://github.com/PagerDuty/scom-powershell-integration
+#
+# Version : 2.0
+# Created : 6/24/2019
+# Modified : 3/2/2021
+#
+# Purpose : This script is used to integrate SCOM command line function to send alerts to PagerDuty
+#
+# Requirements: Ran on computer with the SCOM console installed
+#             
+# Change Log:   Ver 2.0 - (Edited by tcox8) - Added dynamic module importing. 
+#                                           - Added dynamic variables (logfile, routingkey)
+#                                           - Added logic to handle alerts with larger than 1024 characters
+#
+#               Ver 1.0 - Initial release
+#############################################################################
+
+
 Param (
 	[Parameter(Position=0,mandatory=$true)]		[String]$AlertID,
-	[Parameter(Position=1,mandatory=$false)]	[String]$RoutingKey = "ENTER INTEGRATION KEY HERE",
-	[Parameter(Position=2,mandatory=$false)]	[String]$Url = "https://events.pagerduty.com/v2/enqueue"
+	[Parameter(Position=1,mandatory=$false)]	[String]$RoutingKey = "",
+	[Parameter(Position=2,mandatory=$false)]	[String]$Url = "https://events.pagerduty.com/v2/enqueue",
+    	[Parameter(Position=3,mandatory=$false)]    [string]$loglfile = ""
 )
 
-# Import Operations Manager to Retrieve Alert Information
-Import-Module "C:\Program Files\Microsoft System Center 2016\Operations Manager\Powershell\OperationsManager\OperationsManager.psm1"
+#Import the OperationsManager module and connect to the management group
+Try 
+    {
+        #Tested in SCOM 2012 R2 and SCOM 2019 (Update Rollup 2)
+        $SCOMPowerShellKey = "HKLM:\SOFTWARE\Microsoft\System Center Operations Manager\12\Setup\Powershell\V2"
+        $SCOMModulePath = Join-Path (Get-ItemProperty $SCOMPowerShellKey).InstallDirectory "OperationsManager"
+        Import-module $SCOMModulePath -ErrorAction Stop
+    }
+Catch 
+    {
+        Write-Host "Error! Failed to import Ops Manager module! Please make sure the script is running on a computer with the Console installed!"
+    }   
+
 
 # Get Alert Information
 $Alert = Get-SCOMAlert -id $AlertID
@@ -38,8 +72,17 @@ else {"Hostname Not Available"}
 
 # Construct PagerDuty Event Summary
 
+#Have to deal with Alert Summary's greater than 1024 characters due to PagerDuty's limitations
 [String]$AlertSummary = ($Hostname + " - " + $Alert.Name + " - " + $Alert.Description).Trim()
-
+If (($AlertSummary | measure -character).Characters -ge 1023)
+    {
+        $AlertSummary = ($Hostname + " - " + $Alert.Name + " - " + "Details in Custom Fields").Trim()
+        $Summary = $Alert.Description
+    }
+Else
+    {
+        $Summary = "Not Available"
+    }
 
 # Look Up Custom Details
 
@@ -57,6 +100,7 @@ else {"Hostname Not Available"}
 
 # Construct PagerDuty Events Payload
 
+
 $AlertPayload = @{
 	routing_key 			= $RoutingKey
 	event_action 			= $Event
@@ -68,6 +112,7 @@ $AlertPayload = @{
 		timestamp		= $Alert.TimeRaised.ToString("o")
 		custom_details		= @{
 			Priority	= $Priority
+            Summary = $Summary
 			CustomField1 	= $CustomField1
 			CustomField2	= $CustomField2
 			CustomField3 	= $CustomField3
@@ -86,7 +131,7 @@ $AlertPayload = @{
 
 $json = ConvertTo-Json -InputObject $AlertPayload
 
-$logEvents = "C:\scripts\pagerduty\pagerduty_log.txt"
+$logEvents = $logfile
 
 
 # Send to PagerDuty and Log Results
